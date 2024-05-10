0001db3ba6065e0bfbf4ac6efb53830c10472223
IlariaGuarnieri <ilaria.guarnieri@gmail.com>
2024-05-10 14:52:10 +0100

    [add]

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index(){
-        return view('home');
+        //  All’interno della funzione **index()** del controller, recuperate tutti i film dal database e passateli alla view, che quindi li visualizzerà a schermo, tramite delle card.
+        $movies = Movie::all();
+        return view('home', compact('movies'));
     }
 
     public function nuovaPagina(){
